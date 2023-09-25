2e95deace078918c817c24623bc0022c690c5dc9
murilomunari <129402165+murilomunari@users.noreply.github.com>
2023-09-25 12:34:43 -0300

    classe Cliente criada e conexão com o oracle feita

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,7 +1,59 @@
 package br.com.fiap;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static Connection getConnection() throws IOException {
+        Properties prop =  new Properties();
+        FileInputStream file = null;
+        String url = null;
+        String pass = null;
+        String user = null;
+        String driver = null;
+        String debugar = null;
+
+        try {
+           file = new FileInputStream("src/main/resources/application.properties");
+           prop.load(file);
+
+           url = prop.getProperty("datasource.url");
+           user = prop.getProperty("datasource.username");
+           pass = prop.getProperty("datasource.password");
+           driver = prop.getProperty( "datasource.driver-class-name" );
+           debugar = prop.getProperty( "datasource.debugar" );
+           file.close();
+
+           return DriverManager.getConnection(url , pass, user);
+        } catch (FileNotFoundException e) {
+            System.err.println( "Não encontramos o arquivo de configuração: " + e.getMessage() );
+        } catch (IOException e) {
+            System.err.println( "Não foi possível ler o arquivo de configuração: " + e.getMessage() );
+        } catch (SQLException e) {
+            System.err.println( "Não foi possível realizar a conexão com o banco de dados: " + e.getMessage() );
+        }
+        return null;
+    }
+
+    private static final String BASE_URI = "http://localhost/";
+
+    public static HttpServer startServer() {
+
+
+        final ResourceConfig rc = new ResourceConfig()
+                .packages( "br.com.fiap.domain.resource" );
+        return GrizzlyHttpServerFactory.createHttpServer( URI.create( BASE_URI ), rc );
+    }
 
     }
-}
---
+++ src/main/java/br/com/fiap/domain/entity/Cliente.java
@@ -0,0 +1,42 @@
+package br.com.fiap.domain.entity;
+
+public class Cliente {
+
+    private Long id;
+
+    private String nome;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Cliente setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Cliente setNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
