fa99360c2edfd9cf82a6441a2e69f85d57e422e4
murilomunari <129402165+murilomunari@users.noreply.github.com>
2023-11-06 10:54:31 -0300

    Alguns erros (Falar com o professor) //BicicletaService feito

--- src/main/java/br/com/fiap/domain/service/BicicletaService.java
+++ src/main/java/br/com/fiap/domain/service/BicicletaService.java
@@ -1,24 +1,25 @@
 package br.com.fiap.domain.service;
 
 import br.com.fiap.domain.entity.Bicicleta;
+import br.com.fiap.domain.repository.BicicletaRepository;
 
 import java.util.List;
 
 public class BicicletaService implements Service<Bicicleta, Long> {
 
-    Bic
+    private BicicletaRepository repo = BicicletaRepository.build();]
     @Override
     public List<Bicicleta> findAll() {
-        return null;
+        return repo.findAll();
     }
 
     @Override
     public Bicicleta findById(Long id) {
-        return null;
+        return repo.findById(id);
     }
 
     @Override
     public Bicicleta persiste(Bicicleta bicicleta) {
-        return null;
+        return repo.persist(bicicleta);
     }
 }
